09300f40f7ace7ca03c0963e909a965153e60551
sergeytynyanko <109458604+sergeytynyanko@users.noreply.github.com>
2025-09-29 21:56:08 +0300

    Update Task03.py

--- 03_calculator.py
+++ 03_calculator.py
@@ -1,4 +1,35 @@
-# Tasks 3
-....
+# Task3
+
+print('Task 3')
+#Task 1
+name = "sergey"
+day = "monday"
+
+# конкатенація
+print('Task 1')
+print("Good day " + name.title() + "! " + day.title() + " is a perfect day to learn some python.")
+
+# .format()
+print("Good day {}! {} is a perfect day to learn some python.".format(name.title(), day.title()))
+
+# f-рядки
+print(f"Good day {name.title()}! {day.title()} is a perfect day to learn some python.")
+
+#Task 2
+print('Task 2')
+name1 = 'Sergey'
+name2 = 'Tynyanko'
+print('Hello ' + name1 + ' ' + name2 + '!' + '\n')
+
+#Task 3
+print('Task 3')
+a, b = 3, 5
+print('a+b=', a+b)
+print('a-b =', a-b)
+print('a:b=', a/b)
+print('a*b =', a*b)
+print('a**b =', a**b)
+print('[a-b] =', abs(a-b))
+print('a//b =',  a//b)
 
 #
