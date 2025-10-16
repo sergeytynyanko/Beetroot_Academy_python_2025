68bd78fc39f023dd8255f3201ad82a810560eb01
sergeytynyanko <109458604+sergeytynyanko@users.noreply.github.com>
2025-10-16 17:43:33 +0300

    Update main.py

--- Task 9/main.py
+++ Task 9/main.py
@@ -1,1 +1,19 @@
+# урок 9
 
+# 1
+
+# завантажимо функці.
+from func import rectangle1, rectangle2
+
+# задамо висоту та довжину
+height, length = 7, 9
+area = rectangle1(height, length)
+print("Площа прямокутника = ", area)
+
+# підрахуємо периметр
+perimeter = rectangle2(height, length)
+print("Периметр прямокутника = ", perimeter)
+
+
+# Площа прямокутника =  63
+# Периметр прямокутника =  126
